bb29199d3fcefb3060db8ba934ae1267cc9a9db8
VasudhaBhat <vasudhab@straxcorp.com>
2023-05-24 15:34:50 +1000

    test variable replacement inside appsettings

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -21,8 +21,11 @@ namespace autometricsai_backend.Controllers
         [HttpGet(Name = "GetWeatherForecast")]
         public IEnumerable<WeatherForecast> Get()
         {
+            var config = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+            var env = config.GetSection("Env").Value;
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
+                Environ = env,
                 Date = DateTime.Now.AddDays(index),
                 TemperatureC = Random.Shared.Next(-20, 55),
                 Summary = Summaries[Random.Shared.Next(Summaries.Length)]
--- WeatherForecast.cs
+++ WeatherForecast.cs
@@ -2,6 +2,7 @@ namespace autometricsai_backend
 {
     public class WeatherForecast
     {
+        public string? Environ { get; set; }
         public DateTime Date { get; set; }
 
         public int TemperatureC { get; set; }
